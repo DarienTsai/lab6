71fdd5763927d2aa00cb6f8fa359c3071e0af3e2
Darien Tsai <smartdragondt@gmail.com>
2021-01-08 11:37:31 -0800

    lab completed

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,4 +27,19 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	//replace url with heroku when submitting
+	var url = 'http://localhost:3000/project/' + idNumber;
+	$.get(url, addProject);
+}
+
+function addProject(result) {
+	console.log(result);
+	var projectHTML = '<small>' +
+		'<h1>' + result['title'] + '</h1>' +
+		'<p class="date">' + result['date'] +
+    '<img src="' + result['image'] + '" class="img">' +
+		result.summary + '</small>';
+
+	$('#project' +result.id +' .details').html(projectHTML);
+
 }
